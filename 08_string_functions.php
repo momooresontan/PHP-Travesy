0ab4e63a6425e6ec3bb2a64c1f640eb33f7be81c
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-09-11 14:21:51 +0100

    php string functions(format)

--- 08_string_functions.php
+++ 08_string_functions.php
@@ -10,5 +10,12 @@
     echo str_replace("world", "Everyone", $string);
     echo substr($string, 0, 5);
     echo substr($string, 5);
-
+    if(str_starts_with($string, "Hello")){
+        echo "YES<br>";
+    }
+    if(str_ends_with($string, "ld")){
+        echo "YES<br>";
+    }
+    $string2 = "<script>alert(1)</script>";
+    echo htmlspecialchars($string2) . "<br>";
 ?>
