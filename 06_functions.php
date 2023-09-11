db7a47148f3389cc73c1c2c5844eb3b541ac6690
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-09-11 11:55:39 +0100

    php functions(anonymous)

--- 06_functions.php
+++ 06_functions.php
@@ -2,5 +2,16 @@
     function registerUser ($email){
         echo "$email registered";
     }
-    registerUser("momo@test.io");
+    //registerUser("momo@test.io");
+    function sum ($n1 = 4, $n2 = 5){
+        return $n1 + $n2;
+    }
+    $number = sum();
+    // echo $number;
+    $subtract = function ($n1, $n2){
+        return $n1 - $n2;
+    };
+    echo $subtract(10, 5);
+
+
 ?>
