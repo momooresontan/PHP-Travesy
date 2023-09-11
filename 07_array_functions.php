26346f1a074714b1e25cb004bb0581b4ee3ddca0
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-09-11 13:29:38 +0100

    php array functions(merge)

--- 07_array_functions.php
+++ 07_array_functions.php
@@ -11,7 +11,25 @@
     // var_dump(in_array("mango", $fruits));
     //Add to array
     $fruits[] = "grape";
-    array_push($fruits, "mango");    
-    print_r($fruits);
+    array_push($fruits, "mango");
+    array_unshift($fruits, "guava", "avocado");
+    //Remove from array
+    array_pop($fruits);    
+    array_shift($fruits);
+    // unset($fruits[2]);
+    //Split into 2 chunks
+    // $chunked_array = array_chunk($fruits, 2);
+    // print_r($chunked_array);
+    //print_r($fruits);
+    $arr1 = [1, 2, 3];
+    $arr2 = [4, 5, 6];
+    $arr3 = array_merge($arr1, $arr2);
+    $arr4 = [...$arr1, ...$arr2];
+
+    print_r($arr4);
+
+
+
+    
 
 ?>
