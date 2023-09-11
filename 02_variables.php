f5618b785e5a3d12ace1af08b95520a803c849f8
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-09-11 10:08:48 +0100

    php variables

---
+++ 02_variables.php
@@ -0,0 +1,16 @@
+<?php 
+    $name = "MoMo";
+    $age = 22;
+    $hasKids = false;
+    $cashOnHand = 20.75;
+    //echo $name, $age, $hasKids;
+    //var_dump($cashOnHand)
+    // echo "$name is $age years old";
+    // $x = '5' + '5';
+    // var_dump($x);
+
+    define('HOST', 'localhost');
+    define('DB_NAME', 'dev_db');
+
+    echo HOST;
+?>
---
+++ 03_arrays.php
@@ -0,0 +1,9 @@
+<?php
+   //Simple Array
+   $numbers = [1, 44, 32, 24, 50]; 
+    //Array function
+    $fruits = array('apple', 'orange', 'pear');
+
+    var_dump($numbers);
+
+?>
