19edc5dbaf7a014eedce0da032b1de0353600654
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-09-12 22:35:19 +0100

    php file upload

--- 15_file_upload.php
+++ 15_file_upload.php
@@ -1,10 +1,18 @@
 <?php 
     if(isset($_POST["submit"])){
+        $allowed_ext = array("png", "jpg", "jpeg", "gif");
+
         if(!empty($_FILES["upload"]["name"])){
-            print_r($_FILES);
+            //print_r($_FILES);
             $file_name = $_FILES["upload"]["name"];
             $file_size = $_FILES["upload"]["size"];
             $file_tmp = $_FILES["upload"]["tmp_name"];
+            $target_dir = "uploads/$file_name";
+
+            //Get file ext
+            $file_ext = explode(".", $file_name);
+            $file_ext = strtolower(end($file_ext));
+            echo $file_ext;
         } else{
             $message =  '<p style="color: red">Please choose a file</p>';
         }
