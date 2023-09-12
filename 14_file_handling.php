2a6b665da7ca788e82f789e0abff00efda0051be
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-09-12 21:57:45 +0100

    php file handling

--- 14_file_handling.php
+++ 14_file_handling.php
@@ -1,3 +1,13 @@
 <?php 
-    
+    $file = "extras/user.txt";
+
+    if(file_exists($file)){
+        // echo readfile($file);
+        $handle = fopen($file, "r");
+        $contents = fread($handle, filesize($file));
+        fclose($handle);
+        echo $contents;
+    } else{
+        $handle = fopen($file, "w");
+    }
 ?>
