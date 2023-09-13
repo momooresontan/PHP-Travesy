090f46b885808bb843e6fdbe1dcdc4d438ede874
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-09-13 22:49:26 +0100

    php oop

---
+++ 17_oop.php
@@ -0,0 +1,13 @@
+<?php 
+    class User{
+        //Properties are attributes that belong to a class
+        public $name;
+        public $email;
+        public $password;
+
+    }
+    //Instanciate User object
+    $user1 = new User();
+
+    var_dump($user1);
+?>
