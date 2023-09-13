cc638ddd06c24176979700857a82d46f91b43fc5
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-09-13 22:59:53 +0100

    php oop

--- 17_oop.php
+++ 17_oop.php
@@ -4,10 +4,15 @@
         public $name;
         public $email;
         public $password;
+        //Method is a function that belong to a class
+        function set_name($name){
+            $this->name;
+        }
 
     }
     //Instanciate User object
     $user1 = new User();
+    $user1-> name = "MoMo";
 
     var_dump($user1);
 ?>
