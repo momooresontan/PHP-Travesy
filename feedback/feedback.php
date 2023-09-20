82b1ae1035b34b3bbb25a5eaca93b21041c442b7
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-09-20 10:40:03 +0100

    feedback php modified

--- feedback/feedback.php
+++ feedback/feedback.php
@@ -3,41 +3,27 @@
 ?>
 
 <?php 
-  $feedback = [
-    [
-      "id" => 1,
-      "name" => "MoMo",
-      "email" => "momo@test.io",
-      "body" => "MoMo is a King",
-    ],
-    [
-      "id" => 2,
-      "name" => "Murray",
-      "email" => "murray@test.io",
-      "body" => "Murray is a Don",
-    ],
-    [
-      "id" => 3,
-      "name" => "Sammy",
-      "email" => "sammy@test.io",
-      "body" => "Sammy ti fe japa",
-    ],
-  ];
+  $sql = "SELECT * FROM feedback";
+  $result = mysql_query($conn, $sql);
 ?>
         <h2> Past Feedback</h2>
 
         <?php if(empty($feedback)): ?>
           <p class="lead mt3">There is no feedback</p>
         <?php endif; ?>
-
-        <div class="card my-3">
-          <div class="card-body">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
-            molestias animi earum eos dolorem repellat a quibusdam, aperiam vero
-            repellendus voluptatibus natus deserunt sed doloribus inventore,
-            totam labore maxime perferendis!
+        
+        <?php foreach($feedback as $item): ?>
+        <div class="card my-3 w-75">
+          <div class="card-body text-center">
+            <?php 
+              echo $item["body"];
+            ?>
+            <div class="text-secondary mt-2">
+              By <?php echo $item["name"]; ?>
+            </div>
           </div>
         </div>
+        <?php endforeach; ?>
 <?php 
   include 'inc/footer.php';
 ?>
