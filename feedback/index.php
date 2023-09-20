e1f1756402d45dafdaf52262d940ed83d9a2b9af
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-09-20 11:11:19 +0100

    index modified for db post

--- feedback/index.php
+++ feedback/index.php
@@ -6,7 +6,24 @@
   $nameErr = $emailErr = $bodyErr = "";
   //Form submit
   if(isset($_POST["submit"])){
-
+    //Validate name
+    if(empty($_POST["name"])){
+      $nameErr = "Name is required";
+    } else{
+      $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    }
+    //Validate email
+    if(empty($_POST["email"])){
+      $emailErr = "Email is required";
+    } else{
+      $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+    }
+    //Validate body
+    if(empty($_POST["body"])){
+      $bodyErr = "Feedback is required";
+    } else{
+      $body = filter_input(INPUT_POST, "body", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    }
   }
 ?>
         <img src="/PHPBrad/feedback/img/logo.png" class="w-25 mb-3" alt="" />
